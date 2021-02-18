b1be0289c29f61fdb8190c137f0fda5431e6220a
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2021-02-18 12:27:16 -0500

    Update Hockey_Rankings.py

--- Hockey_Rankings.py
+++ Hockey_Rankings.py
@@ -6,6 +6,16 @@ Sources:
     https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
     http://hockeyanalytics.com/2016/07/elo-ratings-for-the-nhl/
 
+    http://www.eloratings.net/system.html
+    https://en.wikipedia.org/wiki/Elo_rating_system
+    https://www.reddit.com/r/hockey/comments/21oo9d/presenting_the_world_hockey_elo_ratings/
+    http://implyingrigged.info/wiki/Elo_Ratings
+    http://clubelo.com/Articles/RatingSystem.html
+    http://lastplanetranking.blogspot.com/2013/11/about.html
+    http://www.baseballprospectus.com/article.php?articleid=5247
+    http://fivethirtyeight.com/features/how-we-calculate-nba-elo-ratings/
+    http://fivethirtyeight.com/datalab/introducing-nfl-elo-ratings/
+
 
 @author: Scott
 """
